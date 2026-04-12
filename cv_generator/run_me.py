4367e13313dbd7079227cdc4814d56289d380bf2
ARENAHO JUSKA <arenahojuska@gmail.com>
2026-04-12 17:23:33 +0200

    pre-pull commit with new project folder

--- cv_generator/run_me.py
+++ cv_generator/run_me.py
@@ -7,4 +7,4 @@ script_path = os.path.join( "muhali_cv_rebuilder_final.py")
 if __name__ == "__main__":
     print(" Launching Muhali AI Test Case Generator.")
 
-    subprocess.run([sys.executable, "-m", "streamlit", "run", script_path])
+    subprocess.run([sys.executable, "-m", "streamlit", "        1run", script_path])
