59bd29cc421b307fd8b53ed9cf50525a06c124b2
arenahojuska <arenahojuska@gmail.com>
2026-04-15 16:25:09 +0200

    Update muhali_cv_rebuilder_final.py
    changed the key

--- cv_generator/muhali_cv_rebuilder_final.py
+++ cv_generator/muhali_cv_rebuilder_final.py
@@ -5,7 +5,7 @@ from groq import Groq
 import io, os, json, re, tempfile, subprocess
 
 # --- 1. CONFIGURATION ---
-GROQ_API_KEY = "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"
+GROQ_API_KEY = "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"
 client = Groq(api_key=GROQ_API_KEY)
 MODEL_ID = "llama-3.3-70b-versatile"
 
