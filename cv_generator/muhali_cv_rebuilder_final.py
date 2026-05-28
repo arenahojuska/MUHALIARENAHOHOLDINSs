43e3f95b1750da0504d66645f76dd927138f6848
arenahojuska <arenahojuska@gmail.com>
2026-05-28 09:31:19 +0200

    Update muhali_cv_rebuilder_final.py

--- cv_generator/muhali_cv_rebuilder_final.py
+++ cv_generator/muhali_cv_rebuilder_final.py
@@ -5,7 +5,7 @@ from groq import Groq
 import io, os, json, re, tempfile, subprocess
 
 # --- 1. CONFIGURATION ---
-GROQ_API_KEY = "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"
+GROQ_API_KEY = "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"
 client = Groq(api_key=GROQ_API_KEY)
 MODEL_ID = "llama-3.3-70b-versatile"
 
